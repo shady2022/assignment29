92d8a0183e1a8047d3077d17f8d034693ae0a254
shady.afsharii20200 <88148144+shady2022@users.noreply.github.com>
2022-01-01 07:55:28 +0330

    Add files via upload

---
+++ logo.py
@@ -0,0 +1,17 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+logo=np.ones((600,1000,3),dtype='uint8')*127
+#cv2.imshow("img", logo)
+
+logo[200:300, 200:300]= (246, 83, 20)
+logo[310:410, 200:300]= (0, 161, 241)
+logo[200:300, 310:410]= (124, 187, 0)
+logo[310:410, 310:410]= (255, 187,0) 
+
+cv2.putText(logo,'Microsoft',(420,300),cv2.FONT_HERSHEY_SIMPLEX,4,(255,255,255),10)
+cv2.imshow("new", logo)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
